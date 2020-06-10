789063aa6536ce09173a5e24db8dc9595b14f233
debaPrasads <66748701+debaPrasads@users.noreply.github.com>
2020-06-11 00:49:30 +0530

    Add files via upload

---
+++ EFCoreAppMigration/DALLayerforList.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreAppMigration
+{
+    class DALLayerforList
+    {
+        CatPrdDbContext ctx;
+        public DALLayerforList()
+        {
+            ctx = new CatPrdDbContext();
+        }
+        public async Task<List<Category>> CreateListAsync(List<Category> cat)
+        {
+            Category cobj = new Category();
+            Product pobj = new Product();
+            foreach (var items in cat)
+            {
+                cobj.CategoryId = items.CategoryId;
+                cobj.BasePrice = items.BasePrice;
+                cobj.CategoryName = items.CategoryName;               
+                await ctx.Categories.AddAsync(cobj);
+                foreach (var products in items.Products)
+                {
+                    if (products.Price >= items.BasePrice)
+                    {
+                        await ctx.Products.AddAsync(products);
+                    }
+
+                }
+
+                await ctx.SaveChangesAsync();
+            }
+            return cat.ToList();
+        }
+
+        public async Task<IEnumerable<PrductViewModel>> GetListAsync(int? id)
+        {
+            
+            var products =  await (from pd in ctx.Products
+                               join od in ctx.Categories on pd.CategoryRowId equals od.CategoryRowId
+                                 where od.CategoryRowId == id
+                               select new PrductViewModel()
+                               {
+                                   ProductId= pd.ProductId,
+                                  ProductName= pd.ProductName,
+                                  Description=pd.Description,
+                                  Price=  pd.Price,
+                                  Manufacturer=pd.Manufacturer,
+                                  CategoryName= od.CategoryName
+                               }).ToListAsync(); 
+            return products;
+        }
+    }
+}
---
+++ EFCoreAppMigration/DalLayer.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreAppMigration
+{
+    public class DalLayer
+    {
+        CatPrdDbContext ctx;
+        public DalLayer()
+        {
+            ctx = new CatPrdDbContext();
+        }
+
+        public async Task<IEnumerable<Category>> GetAsync()
+        {
+            return await ctx.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetAsync(int id)
+        {
+            var cat = await ctx.Categories.FindAsync(id);
+            return cat;
+        }
+        public async Task<Category> CreateAsync(Category cat)
+        {
+            var res = await ctx.Categories.AddAsync(cat);
+            await ctx.SaveChangesAsync();
+            return res.Entity;
+        }
+
+        public async Task<Category> UpdateAsync(int id, Category cat)
+        {
+            var c = await ctx.Categories.FindAsync(id);
+            if (c != null)
+            {
+                // perform upate
+
+                // 1. Update each property separately
+                //c.CategoryId = cat.CategoryId;
+                //c.CategoryName = cat.CategoryName;
+                //c.BasePrice = cat.BasePrice;
+
+                // 2. Update using Cursor State
+                ctx.Entry<Category>(cat).State = EntityState.Modified;
+
+                await ctx.SaveChangesAsync();
+
+            }
+            return cat;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var c = await ctx.Categories.FindAsync(id);
+            if (c != null)
+            {
+                ctx.Categories.Remove(c);
+                await ctx.SaveChangesAsync();
+                return true;
+            }
+            return false;
+            
+        }
+
+    }
+}
